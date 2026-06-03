a55da418a3db310dc9d1b08eee07472e431a09f9
abelytics <abetemitope@gmail.com>
2026-06-03 15:03:23 +0100

    Tableau chart

--- sql_scripts/loading_script/load_order_items_data.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/olist_ecommerce_data/olist_order_items_dataset.csv'
-INTO TABLE order_items
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
--- sql_scripts/loading_script/load_product_category_name_translation_data.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/olist_ecommerce_data/product_category_name_translation.csv'
-INTO TABLE product_category_name_translation
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
